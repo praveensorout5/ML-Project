fbd7b73a8dd8ab800113f6e51a9d503ca95ae726
praveensorout5 <praveen555sorout@gmail.com>
2026-01-29 09:55:31 +0530

    Prediction pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,57 +1,61 @@
 import os
-import sys 
-from src.exception import CustomException 
-from src.logger import logging 
-import pandas as pd 
+import sys
+from src.exception import CustomException
+from src.logger import logging
 
-from sklearn.model_selection import train_test_split 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import datatransformation, datatransformationconfig
-from src.components.model_trainer import modeltrainer
-from src.components.model_trainer import modeltrainerconfig
-@dataclass 
-class dataingestionconfig:
-    train_data_path: str=os.path.join('artifacts', "train.csv")
-    test_data_path: str=os.path.join('artifacts', "test.csv")
-    raw_data_path: str=os.path.join('artifacts', "data.csv")
-    
-class dataingestion:
-    def __init__(self):
-        self.ingestion_config=dataingestionconfig()
-        
-    def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
-        try:
-            df = pd.read_csv("")
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
-            logging.info('Read the datasets as dataframe')
+@dataclass
+class DataIngestionConfig:
+    train_data_path : str=os.path.join('artifacts','train.csv')
+    test_data_path : str=os.path.join('artifacts','test.csv')
+    raw_data_path : str=os.path.join('artifacts','data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered DataIngestion")
+
+        try:
+            df=pd.read_csv('/config/workspace/ML-projects/notebook/data/stud.csv')
+            logging.info('Dataset to DataFrame.')
+
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            
-            logging.info("Train test split initiated")
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("train test splitting...")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
             
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
-            
-            logging.info("Ingestion of the data is completed")
-            
+            logging.info("data ingestion completed..")
+
             return(
+
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            raise CustomException(e,sys)
-        
+            raise CustomException(e, sys)
+
 if __name__=="__main__":
-    obj=dataingestion()
+    obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-    
-    data_transformation=datatransformation() 
-    train_arr,test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
-    
-    modeltrainer=modeltrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
